348bf9fe96ddbad2612ebb50d551b5f91b81a376
Marc G Fournier <scrappy@hub.org>
2022-11-24 00:20:44 -0800

    Start mapping table for iphone id -> model name

--- FreeAPS/Sources/Helpers/UIDevice+Extensions.swift
+++ FreeAPS/Sources/Helpers/UIDevice+Extensions.swift
@@ -10,7 +10,30 @@ extension UIDevice {
             return identifier + String(UnicodeScalar(UInt8(value)))
         }
 
-        return identifier
+        func mapToDevice(identifier: String) -> String {
+            switch identifier {
+            case "iPhone12,1":
+                return "iPhone 11"
+            case "iPhone12,8":
+                return "iPhone SE (2nd Gen)"
+
+            case "iPhone13,4":
+                return "iPhone 12 Pro Max"
+
+            case "iPhone14,4":
+                return "iPhone 13 mini"
+            case "iPhone14,6":
+                return "iPhone SE (3rd Gen)"
+
+            case "iPhone15,2":
+                return "iPhone 14 Pro"
+
+            default:
+                return identifier
+            }
+        }
+
+        return mapToDevice(identifier: identifier)
     }
 
     var getOSInfo: String {
