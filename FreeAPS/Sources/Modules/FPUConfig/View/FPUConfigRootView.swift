cee5f524ef1f0b95aa0f898bfcfdd9778c8c87f6
Robert Silvers <rsilvers@photomosaic.com>
2023-02-25 18:41:44 -0500

    Update FPU Settings Text.

--- FreeAPS/Sources/Modules/FPUConfig/View/FPUConfigRootView.swift
+++ FreeAPS/Sources/Modules/FPUConfig/View/FPUConfigRootView.swift
@@ -26,27 +26,27 @@ extension FPUConfig {
                     Toggle("Enable", isOn: $state.useFPUconversion)
                 }
 
-                Section(header: Text("Optional conversion settings")) {
+                Section(header: Text("Conversion settings")) {
                     HStack {
-                        Text("Maximum Time Cap In Hours")
+                        Text("Maximum Duration In Hours")
                         Spacer()
                         DecimalTextField("8", value: $state.timeCap, formatter: intFormater)
                     }
                     HStack {
-                        Text("Interval In Minutes")
+                        Text("Interval In Minutes:")
                         Spacer()
-                        DecimalTextField("60", value: $state.minuteInterval, formatter: intFormater)
+                        DecimalTextField("20", value: $state.minuteInterval, formatter: intFormater)
                     }
                     HStack {
-                        Text("Override with a factor of ")
+                        Text("Adjustment factor:")
                         Spacer()
-                        DecimalTextField("0.8", value: $state.individualAdjustmentFactor, formatter: conversionFormatter)
+                        DecimalTextField("0.5", value: $state.individualAdjustmentFactor, formatter: conversionFormatter)
                     }
                 }
 
                 Section(
                     footer: Text(
-                        "Allows fat and protein to be converted to future carb equivalents.\n\nDefault settings:\n\nTime Cap: 8 hours\nInterval: 60 min\nFactor: 0.5"
+                        "Allows fat and protein to be converted to future carb equivalents using a formula of kilocalories divided by 10.\n\nThis spreads the carb equivilants over a maximum duration setting that can be configured from 8-12 hours.\n\nInterval in minutes is how many minutes are between entries. The shorter the interval, the smoother the results, but it becomes harder to manually delete entries if you later want to remove them. 5, 10, 15, 20, 30, or 60 are reasonable choices.\n\nAdjustment factor is how much effect the fat and protein has on the entries. 1.0 is full effect and 0.5 is half effect.\nNote that you may find that your normal carb ratio needs to increase to a larger number if you begin adding fat and protein entries. For this reason, it is best to start with a factor of about 0.5 to ease into it.\n\nDefault settings:\n\nTime Cap: 8 hours\nInterval: 20 min\nFactor: 0.5"
                     )
                 )
                     {}
